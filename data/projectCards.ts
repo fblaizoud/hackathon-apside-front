3ff9220e8c6b4e18e211ac287e73cd54375f8c7c
unknown <mrbreenalexis@gmail.com>
2022-06-30 13:58:13 +0200

    J'envoi le jus

---
+++ data/projectCards.ts
@@ -0,0 +1,58 @@
+const projectCards = [
+  {
+    id: 1,
+    project: 'https://i.ibb.co/YPGVhWP/logo-2.png',
+    employees: [
+      {
+        src: './assets/person1.png',
+        name: 'Elton John',
+        jobTitle: 'Sir',
+      },
+      {
+        src: './assets/person2.png',
+        name: 'Madonna',
+        jobTitle: 'Singer',
+      },
+      {
+        src: './assets/person3.png',
+        name: 'PPDA',
+        jobTitle: 'Journalist',
+      },
+      {
+        src: './assets/person4.png',
+        name: 'Jimmy',
+        jobTitle: 'Web dev',
+      },
+    ],
+    progress: 'https://i.ibb.co/C9tvwDz/Circle.png',
+  },
+  {
+    id: 2,
+    project: 'https://i.ibb.co/YPGVhWP/logo-2.png',
+    employees: [
+      {
+        src: './assets/person5.png',
+        name: 'Paul',
+        jobTitle: 'Emploi',
+      },
+      {
+        src: './assets/person3.png',
+        name: 'Gilbert',
+        jobTitle: 'Singer',
+      },
+      {
+        src: './assets/person1.png',
+        name: 'John Cenna',
+        jobTitle: 'Journalist',
+      },
+      {
+        src: './assets/person4.png',
+        name: 'El chico',
+        jobTitle: 'Web dev',
+      },
+    ],
+    progress: 'https://i.ibb.co/PMDTkcq/Circle.png',
+  },
+];
+
+export default projectCards;
