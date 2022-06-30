d18a941935c53f305de4668a6c69e504c64b60c9
Lucas ARRIZABALAGA <arrizabalucas@gmail.com>
2022-06-30 11:10:04 +0200

    navBar inti

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,14 @@ import React, { useContext } from 'react';
 import { BrowserRouter as Router, Link, Route, Routes } from 'react-router-dom';
 
 import Home from './components/Home';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
-          <Route path="*" element={<Home />} />
+          <Route path="*" element={<Navbar />} />
         </Routes>
       </Router>
     </div>
